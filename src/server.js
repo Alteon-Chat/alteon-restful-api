7186da1285222fc6116ff29daf5d0cabe99e8a25
Irwan Gumilar <irwangumilar111@gmail.com>
2022-07-13 19:02:24 +0700

    .

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const DB = process.env.DATABASE;
 
 mongoose.connect(DB).then(() => console.log("Database connection succesfull!"));
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
